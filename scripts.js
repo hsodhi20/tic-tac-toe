3636bcb466bb26cac9bb171bb8dd5f551821820d
hsodhi20 <hsodhi2003@gmail.com>
2024-04-20 12:52:28 +0530

    working tic tac toe

--- scripts.js
+++ scripts.js
@@ -1,76 +1,82 @@
-function Cell() {
-    let value = '-';
-
-    const markCell = (player) => value = player;
-    const getValue = () => value;
-    return {
-        markCell,
-        getValue
-    };
-}
-
-function GameBoard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
-
-    for(let i = 0; i < rows; i ++) {
-        board[i] = [];
-        for(let j = 0; j < columns; j ++) {
-            board[i].push(Cell());
-        }
+class Cell {
+    constructor() {
+        this._value = '-';
     }
 
-    const getBoard = () => board;
+    get value() {
+        return this._value;
+    }
 
-    const playTurn = (player, row, column) => {
-        if(board[row][column].getValue() === '-') {
-            board[row][column].markCell(player);
+    set value(newValue) {
+        this._value = newValue;
+    }
+}
+
+class GameBoard {
+    constructor() {
+        this.rows = 3;
+        this.columns = 3;
+        this.board = this.createBoard();
+    }
+
+    createBoard() {
+        const board = [];
+        for(let i = 0; i < this.rows; i ++) {
+            board[i] = [];
+            for(let j = 0; j < this.columns; j ++) {
+                board[i].push(new Cell());
+            }
+        }
+        return board;
+    }
+    
+    getBoard() {
+        return this.board;
+    }
+
+    playTurn(player, row, column) {
+        if(this.board[row][column].value === '-') {
+            this.board[row][column].value = player;
             return true;
         }
         else {
             return false;
         }
-    };
-
-    const printBoard = () => {
-        const curState = board.map((row) => row.map((cell) => cell.getValue()));
-        console.log(curState);
     }
 
-    return { getBoard, playTurn, printBoard };
-}
+    printBoard() {
+        const curState = this.board.map(row => row.map(cell => cell.value));
+        console.log(curState);
+    }
+} 
 
-function GameController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-) {
-    const board = GameBoard();
-
-    const players = [
-        {
-            name: playerOneName,
-            token: 'x'
-        },
-        {
-            name: playerTwoName,
-            token: 'o'
+class GameController {
+    constructor(
+        playerOneName = "Player One",
+        playerTwoName = "Player Two") {
+            this.board = new GameBoard();
+            this.players = [
+                {name: playerOneName, token: 'x'},
+                {name: playerTwoName, token: 'o'}
+            ];
+            this.activePlayer = this.players[0];
+            this.printNewRound();
         }
-    ];
-     
-    let activePlayer = players[0];
 
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-    const getActivePlayer = () => activePlayer;
+    switchPlayerTurn() {
+        this.activePlayer = this.activePlayer === this.players[0] ? this.players[1] : this.players[0]; 
+    }
 
-    const printNewRound = () => {
-        board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn.`);
-    };
+    getActivePlayer() {
+        return this.activePlayer;
+    }
 
-    const checkBoard = () => {
+    printNewRound() {
+        this.board.printBoard();
+        console.log(`${this.getActivePlayer().name}'s turn`);
+    }
+
+    checkBoard() {
         let emptyCells = 0;
 
         // Check rows
@@ -78,10 +84,10 @@ function GameController(
             let rowMatches = 0;
             for(let j = 0; j < 3; j ++) {
                 // Corrected: Added parentheses to call getValue() function
-                if(board.getBoard()[i][j].getValue() === '-') {
+                if(this.board.getBoard()[i][j].value === '-') {
                     emptyCells ++;
                 }
-                else if(board.getBoard()[i][j].getValue() === board.getBoard()[i][0].getValue()) {
+                else if(this.board.getBoard()[i][j].value === this.board.getBoard()[i][0].value) {
                     rowMatches ++;
                 }
             }
@@ -95,7 +101,7 @@ function GameController(
             let colMatches = 0;
             for(let j = 0; j < 3; j ++) {
                 // Corrected: Added parentheses to call getValue() function
-                if(board.getBoard()[j][i].getValue() === board.getBoard()[0][i].getValue() && board.getBoard()[i][j].getValue() != '-') {
+                if(this.board.getBoard()[j][i].value === this.board.getBoard()[0][i].value && this.board.getBoard()[i][j].value !== '-') {
                     colMatches ++;
                 }
             }
@@ -106,10 +112,10 @@ function GameController(
 
 
         // Check diagonals
-        if((board.getBoard()[0][0].getValue() != '-' &&
-            board.getBoard()[0][0].getValue() === board.getBoard()[1][1].getValue() && board.getBoard()[1][1].getValue() === board.getBoard()[2][2].getValue()) ||
-            (board.getBoard()[0][2].getValue() != '-') && 
-            board.getBoard()[0][2].getValue() === board.getBoard()[1][1].getValue() && board.getBoard()[1][1].getValue() === board.getBoard()[2][0].getValue()) 
+        if((this.board.getBoard()[0][0].value !== '-' &&
+            this.board.getBoard()[0][0].value === this.board.getBoard()[1][1].value && this.board.getBoard()[1][1].value === this.board.getBoard()[2][2].value) ||
+            (this.board.getBoard()[0][2].value !== '-' && 
+            this.board.getBoard()[0][2].value === this.board.getBoard()[1][1].value && this.board.getBoard()[1][1].value === this.board.getBoard()[2][0].value)) 
             {
             return true;
             }
@@ -122,33 +128,83 @@ function GameController(
         }
 
         return false;
-    };
+    }
         
-    
-    const playRound = (row, column) => {
-        console.log(`Player ${getActivePlayer().name} row: ${row}, column: ${column}`);
-        board.playTurn(getActivePlayer().token, row, column);
-      
-            const checker = checkBoard();
-            if(checker === true) {
-                console.log(`${getActivePlayer().name} won the game`);
-            }
-            else if(checker === 'tie') {
-                console.log(`It's a tie`);
-            }
-            else {
-                switchPlayerTurn();
-                printNewRound();
-            }
-      
-        
-    };
 
-    printNewRound();
-    return {
-        playRound,
-        getActivePlayer
-    };
+    playRound(row, column) {
+        console.log(`Player ${this.getActivePlayer().name} row: ${row}, column: ${column}`);
+        if (this.board.playTurn(this.getActivePlayer().token, row, column)) {
+            const checker = this.checkBoard();
+            if (checker === true) {
+                console.log(`${this.getActivePlayer().name} won the game`);
+            } else if (checker === 'tie') {
+                console.log(`It's a tie`);
+            } else {
+                this.switchPlayerTurn();
+                this.printNewRound();
+            }
+        } else {
+            console.log("Retry!");
+            this.printNewRound();
+        }
+    }
+    
 }
 
-const game = GameController();
+const game = new GameController();
+
+function initializeBoard() {
+    const boardContainer = document.querySelector('.board');
+    const board = game.board.getBoard();
+
+    while (boardContainer.firstChild) {
+        boardContainer.removeChild(boardContainer.firstChild);
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        
+        for (let j = 0; j < board[i].length; j++) {
+            const cellDiv = document.createElement('div');
+            cellDiv.classList.add('cell');
+            cellDiv.setAttribute('data-row', i);
+            cellDiv.setAttribute('data-column', j);
+            cellDiv.textContent = board[i][j].value;
+            rowDiv.appendChild(cellDiv);
+        }
+
+        boardContainer.appendChild(rowDiv);
+    }
+}
+
+initializeBoard();
+
+document.querySelectorAll('.cell').forEach(cell => {
+    cell.addEventListener('click', () => {
+        const row = parseInt(cell.getAttribute('data-row'));
+        const column = parseInt(cell.getAttribute('data-column'));
+        game.playRound(row, column);       
+        updateInterface();
+    });
+});
+
+function updateInterface() {
+    const board = game.board.getBoard();
+    const resultDisplay = document.querySelector('.turn');
+
+    document.querySelectorAll('.cell').forEach((cell, index) => {
+        const row = Math.floor(index / board.length);
+        const column = index % board[row].length;
+        cell.textContent = board[row][column].value;
+    });
+
+    const checker = game.checkBoard();
+    if (checker === true) {
+        resultDisplay.textContent = `${game.getActivePlayer().name} won the game`;
+    } else if (checker === 'tie') {
+        resultDisplay.textContent = `It's a tie`;
+    } else {
+        resultDisplay.textContent = `${game.getActivePlayer().name}'s turn`;
+    }
+}
